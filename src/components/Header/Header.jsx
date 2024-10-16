96b98e679487da3e8ef820356161a2f51597700c
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2024-10-16 12:15:32 -0300

    Components em React

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+
+import './Header.css';
+
+function Header() {
+    return(
+        <>
+            <header>
+                <div>
+                    Two <span>Training</span>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">home</a></li>
+                        <li><a href="#">produtos</a></li>
+                        <li><a href="#">serviços</a></li>
+                        <li><a href="#">contato</a></li>
+                        <li><a href="#">sobre</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+import Header from '../../components/Header/Header';
+
+// Página HOME
+function Home() {
+    return(
+        <>
+            <Header></Header>
+        </>
+    );
+}
+
+export default Home;
